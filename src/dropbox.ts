a25180ad66fdbfcc4e17a77fab59d705378f4a11
delphinus <delphinus@remora.cx>
2018-10-28 19:57:59 +0900

    Add `fetch` instance for Dropbox

--- src/dropbox.ts
+++ src/dropbox.ts
@@ -1,9 +1,16 @@
-import { Dropbox } from "dropbox"
+import { Dropbox, DropboxOptions } from "dropbox"
+import fetch from "isomorphic-fetch"
 import { dropboxAccessToken } from "./secrets.json"
 import { SlackMessage } from "./slack"
 
 export const saveMessage = async (message: SlackMessage) => {
-    const dropbox = new Dropbox({ accessToken: dropboxAccessToken })
+    // TODO: DropboxOptions lacks the `fetch` property.  Has the type mistaken?
+    /* tslint:disable:no-object-literal-type-assertion */
+    const dropbox = new Dropbox({
+        accessToken: dropboxAccessToken,
+        fetch,
+    } as DropboxOptions)
+    /* tslint:enable:no-object-literal-type-assertion */
     return dropbox.filesUpload({
         contents: message.ts,
         path: `/${message.text.substring(0, 100).replace(/\s/g, "-")}.txt`,
